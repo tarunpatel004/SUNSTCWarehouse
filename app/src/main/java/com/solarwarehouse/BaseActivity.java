01e034ecf3a181ad9e2b34e221239a7644e5d76f
tarunpatel004 <tarunpatel004@gmail.com>
2018-11-09 21:31:12 +0530

    initial commit

---
+++ app/src/main/java/com/solarwarehouse/BaseActivity.java
@@ -0,0 +1,18 @@
+package com.solarwarehouse;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Dell on 10/21/2018.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
